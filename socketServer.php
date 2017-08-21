2f299a5ac4bc61b47eab03d437b7f4271ed7ee9f
soonjone <soonjone@qq.com>
2017-08-21 15:39:32 +0800

    mod

--- socketServer.php
+++ socketServer.php
@@ -18,6 +18,19 @@ $serv->on('receive', function ($serv, $fd, $from_id, $data) {
     $serv->send($fd, "Server: " . $data);
 });
 
+
+//处理异步任务
+$serv->on('task', function ($serv, $task_id, $from_id, $data) {
+    echo "New AsyncTask[id=$task_id]".PHP_EOL;
+    //返回任务执行的结果
+    $serv->finish("$data -> OK");
+});
+
+//处理异步任务的结果
+$serv->on('finish', function ($serv, $task_id, $data) {
+    echo "AsyncTask[$task_id] Finish: $data".PHP_EOL;
+});
+
 //监听连接关闭事件
 $serv->on('close', function ($serv, $fd) {
     echo "Client: Close.\n";
